bb2122e5bcf500fab468dd8a59418b79d473941f
Vishal27-com <vishalgupta962148@gmail.com>
2023-04-29 16:51:08 +0530

    database added

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+require("dotenv").config()
 const express = require('express')
 const app = express()
+const dbConnect=require('./Utils/db')
 const cors=require("cors");
 app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-
+dbConnect();
 app.get('/', (req, res) => res.send('hello'))
 
 app.listen(8080, () => {console.log('server started on port 8080')})
