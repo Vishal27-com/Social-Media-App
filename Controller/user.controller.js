94669c13b472056639e03e84d68fa033bd161fd8
Vishal27-com <vishalgupta962148@gmail.com>
2023-04-30 11:40:45 +0530

    user controller

---
+++ Controller/user.controller.js
@@ -0,0 +1,45 @@
+const User =require('../Model/user.model')
+const getAllUser=async(req,res)=>{
+    try {
+      const users=await User.find()
+      res.status(200).send({message:users,error:false})
+    } catch (error) {
+        res.status(500).send({message:error.message,error:true})
+    }
+}
+const getFriendsOfUser=async(req,res)=>{
+    try {
+       const friends=await User.find({_id:req.params.id},{friends:1})
+       res.status(200).send({message:friends,error:false}) 
+    } catch (error) {
+        res.status(500).send({message:error.message,error:true})
+    }
+}
+const sendFriendRequest=async(req,res)=>{
+    try {
+        const from=req.body.from;
+        const to=req.params.id;
+        await User.updateOne({_id:to},{$push:{friendRequests:from}})
+        res.status(200).send({message:"Request send",error:false})
+    } catch (error) {
+        res.status(500).send({message:error.message,error:true})
+    }
+}
+const acceptOrRejectRequest=async(req,res)=>{
+    try {
+        const {friendId}=req.params
+        const userId=req.params.id
+        if(req.body.status==='accept'){
+            await User.updateOne({_id:userId},{$push:{friends:friendId}})
+            await User.updateOne({_id:userId},{$pull:{friendRequests:friendId}})
+            res.status(200).send({message:"Request accepted",error:false})
+        }
+        else{
+            await User.updateOne({_id:userId},{$pull:{friendRequests:friendId}})
+            res.status(200).send({message:"Request rejected",error:false})
+        }
+    } catch (error) {
+        res.status(500).send({message:error.message,error:true})
+    }
+}
+module.exports={getAllUser,getFriendsOfUser,sendFriendRequest,acceptOrRejectRequest}
