c6dcf46628f7edc1c7daf1ec7a529a8e72805524
Vishal27-com <vishalgupta962148@gmail.com>
2023-04-30 11:41:40 +0530

    auth and user route added

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const express = require('express')
 const app = express()
 const dbConnect=require('./Utils/db')
 const cors=require("cors");
+const authRouter=require('./Routes/auth.routes')
+const userRouter=require('./Routes/user.routes')
+
 app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 dbConnect();
 app.get('/', (req, res) => res.send('hello'))
-
+app.use('/api',authRouter)
+app.use('/api/users',userRouter)
 app.listen(8080, () => {console.log('server started on port 8080')})
